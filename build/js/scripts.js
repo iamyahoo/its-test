f6f01661c7e54fb2749cfa41bd0724dc7d14064a
iamyahoo <rdkuznetsov@gmail.com>
2016-01-13 21:23:41 +0300

    верстка, адаптив, яндекс-карта

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,31 +1,63 @@
 $(document).ready(function(){
 
 	//yandex maps
-	ymaps.ready(function () {
-	    var myMap = new ymaps.Map('map', {
-	            center: [59.93772, 30.313622],
-	            zoom: 10,
-	            controls: ['zoomControl']
-	        });
-	});
+	ymaps.ready(init);
+	var myMap;
+
+	function init () {
+	    myMap = new ymaps.Map("map", {
+	        center: [59.93772, 30.313622],
+	        zoom: 11
+	    }, {
+	        balloonMaxWidth: 250
+	    });
+
+	    //кнопки масштабирования
+	    myMap.controls.add('zoomControl', {
+	    	right: 20,
+	    	bottom: 20
+	    }) ;
+
+	    // Обработка события, возникающего при щелчке
+	    // левой кнопкой мыши в любой точке карты.
+	    // При возникновении такого события откроем балун.
+	    myMap.events.add('click', function (e) {
+	        if (!myMap.balloon.isOpen()) {
+	            var coords = e.get('coordPosition');
+	            myMap.balloon.open(coords, {
+	            	baloonShadow: false,
+	                contentBody:
+	                	'<p class="ymaps-baloon-title">Курортный район</p>' +
+	                    '<a class="ymaps-baloon-link">Поселок песочный</a>'
+	            });
+	        }
+	        else {
+	            myMap.balloon.close();
+	        }
+	    });
+	}
+
 
 
 	//page-menu opener
-	$(function (){
+	$(function (pageMenuOpen) {
+		var pageMenu = $('.page-menu-wrapper');
 		var pageMenuIcon = $('.page-menu__opener-icon');
-		var pageMenu = $('.page-menu__item-wrapper');
+		var pageMenuWrapper = $('.page-menu__item-wrapper');
 		
 		$(pageMenuIcon).click(function() {
     		$(this).toggleClass('page-menu__opener-icon_active'); 
-    		$(pageMenu).toggleClass('page-menu__item-wrapper_active');   		
+    		$(pageMenu).toggleClass('page-menu-wrapper_active');
+    		$(pageMenuWrapper).toggleClass('page-menu__item-wrapper_active');   		
     	});	
 	})
 
-	// compute window height
-	$(function (){
-		var mapHeight= $('.page-container').height;
-		console.log(mapHeight);
-	})
-	
+	$(function (headerMenuOpen) {
+		var headerMenuIcon = $('.header-menu__opener-icon');
+
+		$(headerMenuIcon).click(function() {
+    		$(this).toggleClass('header-menu__opener-icon_active');
+    	});
+    });
 
 })
